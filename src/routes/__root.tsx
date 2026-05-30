e5e09d8995a37155fa0711817d008037480e691b
Muhammed Elyürek <muhammed.elyurek@hipposoft.net>
2026-05-30 08:08:51 +0300

    data layer

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -90,7 +90,8 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
   errorComponent: ErrorComponent,
 });
 
-const GTM_SCRIPT = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+const GTM_SCRIPT = `window.dataLayer=window.dataLayer||[];window.dataLayer.push({tool:'json-tools'});
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
